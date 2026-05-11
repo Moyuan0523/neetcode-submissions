80d6b87c5a033c30f4a2cf2a2c34679a156d43a1
Moyuan0523 <121845445+Moyuan0523@users.noreply.github.com>
2026-05-11 22:39:26 +0800

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int goal = n - 1;
+        for(int i = n - 2; i > -1; i--){
+            if(nums[i] >= goal - i) goal = i;
+        }
+        if(goal == 0) return true;
+        else return false;
+    }
+};
