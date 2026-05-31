c273aa081a4cd4998b43d41fdb37ba8e7c2e1405
Moyuan0523 <121845445+Moyuan0523@users.noreply.github.com>
2026-05-31 15:38:52 +0800

    Add: valid-binary-search-tree - submission-5

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-5.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// 以 inorder 方式一定為遞增，以 prev 方式記錄
+class Solution {
+private:
+    TreeNode* prev = nullptr;
+public:
+    bool isValidBST(TreeNode* root) {
+        if(!root) return true;
+        if(!isValidBST(root->left)) return false;
+        if(prev != nullptr && root->val <= prev->val) return false;
+        prev = root;
+        return isValidBST(root->right);
+    }
+};
