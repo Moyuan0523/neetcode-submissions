5236f56d249973da2acd7daed4ac9dc36ab1ac8f
Moyuan0523 <121845445+Moyuan0523@users.noreply.github.com>
2026-06-01 20:05:21 +0800

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.cpp
@@ -0,0 +1,29 @@
+// 輪流當頭，小的先進來
+// edge case: 111111118
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void combine(vector<int>& nums, int target, int index){
+        if(target == 0){
+            result.push_back(path);
+        }
+        else if(target < 0){
+            return;
+        }
+        else{
+            for(int i = index; i < nums.size(); i++){
+                path.push_back(nums[i]);
+                combine(nums, target - nums[i], i);
+                path.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        combine(sorted_nums, target, 0);
+        return result;
+    }
+};
